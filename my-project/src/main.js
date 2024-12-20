723f193bee67839ade4612c79307a6a688495317
Gabrieln999 <155887339+Gabrieln99@users.noreply.github.com>
2024-12-20 18:21:08 +0100

    vue+ tailwind uspjesno

---
+++ my-project/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
+
+/* paziti na ovo:
+import "./assets/tailwind.css"; // Ovo mora odgovarati stvarnoj lokaciji fajla*/
